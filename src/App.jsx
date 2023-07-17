caf43d0938ebe3287a7c29f6b2126708b913dc5d
Andreia <robescu.andreia@gmail.com>
2023-07-17 19:14:28 +0200

    add title, 3 key points and main description

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { ChakraProvider, Button } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Button,
+  ListItem,
+  UnorderedList,
+} from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
     <ChakraProvider>
+      <div>
+        <h1>Menu Semanal</h1>
+        <h2>
+          <UnorderedList>
+            <ListItem>Tiempo de preparacion: menos de 15 minutos </ListItem>
+            <ListItem>Comer saludable: platos frescos y al horno</ListItem>
+            <ListItem>Pocos ingredientes: maximo 5 per plato</ListItem>
+          </UnorderedList>
+        </h2>
+        <h3>
+          El menu semanal definitivo, para los que no quieren perder el tiempo
+          en la cocina. Equilibrado y facil, con ingredientes que encuentras en
+          cualquier supermercado. Nuestra filosfofia: Comer pizza no hara que
+          engordes. No hacer deporte si.{" "}
+        </h3>
+      </div>
       <Button colorScheme="blue">Button</Button>
     </ChakraProvider>
   );
