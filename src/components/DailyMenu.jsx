a335883e4a004222fc2e387e6ddddf4fae99b402
Andreia <robescu.andreia@gmail.com>
2023-07-30 20:53:02 +0200

    add image

--- src/components/DailyMenu.jsx
+++ src/components/DailyMenu.jsx
@@ -1,4 +1,4 @@
-import { Box, Text, Stack } from "@chakra-ui/react";
+import { Box, Text, Stack, Image } from "@chakra-ui/react";
 
 export function DailyMenu({ day }) {
   return (
@@ -6,6 +6,9 @@ export function DailyMenu({ day }) {
       <Text mb="8px" fontSize="xl">
         {day.dayOfTheWeek}
       </Text>
+      <Box boxSize="sm">
+        <Image src="https://img.buzzfeed.com/thumbnailer-prod-us-east-1/d7f0a66959f544f5b885f3bd993f072f/BFV43733_ChowMein4Ways_FB1080SQ.jpg" />
+      </Box>
       <Stack mb="16px" spacing={1}>
         {day.meals.map(({ name, description }) => (
           <Text fontSize="sm">
