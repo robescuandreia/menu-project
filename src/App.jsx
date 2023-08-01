c4870c3ee6f151c8451453aedd83f4a3d4965d42
Andreia <robescu.andreia@gmail.com>
2023-08-01 13:34:52 +0200

    add ingredients in meals

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,34 @@ function App() {
         {
           name: "Desayuno",
           description: "tostada con tomate",
+          ingredients: ["pan", "tomates"],
         },
         {
           name: "Comida",
           description:
             "pollo teriyaki al horno, con arroz, tomate cherry y mango",
+          ingredients: [
+            "pechuga de pollo",
+            "salsa teriyaki",
+            "arroz",
+            "tomate cherry",
+            "mango",
+          ],
         },
         {
           name: "Cena",
           description:
             "ensalada de aguacate, tomate y paraguayo con pollo rebozado",
+          ingredients: [
+            "lechuga",
+            "tomates",
+            "aguacates",
+            "pechuga de pollo",
+            "paraguayos",
+            "huevos",
+            "harina",
+            "pan rallado",
+          ],
         },
       ],
     },
@@ -37,14 +55,22 @@ function App() {
         {
           name: "Desayuno",
           description: "tostada con mantequilla y marmelada",
+          ingredients: ["pan", "mantequilla", "marmelada"],
         },
         {
           name: "Comida",
           description: "hamburguesa de angus con mango y queso chedar fundido",
+          ingredients: [
+            "pan hamburguesa",
+            "hamburguesa angus",
+            "mango",
+            "queso chedar",
+          ],
         },
         {
           name: "Cena",
           description: "ensalada de atun, queso, tomate y maiz",
+          ingredients: ["lechuga", "tomates", "atun", "queso", "maiz"],
         },
       ],
     },
@@ -54,15 +80,34 @@ function App() {
         {
           name: "Desayuno",
           description: "galletas de avena y manzana con kiwi y naranja",
+          ingredients: [
+            "copos de avena",
+            "manzanas golden",
+            "kiwis",
+            "naranjas",
+            "aceite girasol",
+            "esencia vainilla",
+            "azucar",
+            "harina",
+            "levadura",
+          ],
         },
         {
           name: "Comida",
           description:
             "pizza marguerita con albahaca, tomate cherry y aceitunas",
+          ingredients: [
+            "pizza marguerita",
+            "tomate cherry",
+            "albahaca",
+            "aceitunas",
+            "ketchup",
+          ],
         },
         {
           name: "Cena",
           description: "pavo con calabacin y pina al horno",
+          ingredients: ["pavo", "calabacines", "pina", "tomate frito"],
         },
       ],
     },
@@ -72,14 +117,29 @@ function App() {
         {
           name: "Desayuno",
           description: "leche con cereales y nueces",
+          ingredients: [
+            "leche",
+            "bebida avena",
+            "copos de avena",
+            "nueces",
+            "cereales choco",
+            "corn flakes",
+          ],
         },
         {
           name: "Comida",
           description: "pasta con salsa bolonesa, tomate cherry y parmesano",
+          ingredients: [
+            "pasta",
+            "salsa bolonesa",
+            "tomate cherry",
+            "parmesano",
+          ],
         },
         {
           name: "Cena",
           description: "patatas, zanahorias, champinones y calabacin al horno",
+          ingredients: ["patatas", "zanahorias", "champinones", "calabacines"],
         },
       ],
     },
@@ -89,14 +149,17 @@ function App() {
         {
           name: "Desayuno",
           description: "tostada con tomate",
+          ingredients: ["pan", "tomates"],
         },
         {
           name: "Comida",
           description: "pizza barbacoa",
+          ingredients: ["pizza barbacoa", "ketchup"],
         },
         {
           name: "Cena",
           description: "patatas, zanahorias, broccoli y pimiento al horno",
+          ingredients: ["patatas", "zanahorias", "broccoli", "pimientos"],
         },
       ],
     },
@@ -106,14 +169,24 @@ function App() {
         {
           name: "Desayuno",
           description: "tostada con mantequilla y marmelada",
+          ingredients: ["pan", "mantequilla", "marmelada"],
         },
         {
           name: "Comida",
           description: "parmigiana",
+          ingredients: [
+            "berenjena",
+            "albahaca",
+            "salsa de tomate",
+            "mozarella",
+            "harina",
+            "parmesano",
+          ],
         },
         {
           name: "Cena",
           description: "picoteo de gazpacho, bocadillo jamon, hummus y queso",
+          ingredients: ["pan", "gazpacho", "hummus", "queso", "jamon serrano"],
         },
       ],
     },
@@ -122,21 +195,42 @@ function App() {
       meals: [
         {
           name: "Desayuno",
-          description: "bizcocho o brownie con yogur griego",
+          description: "bizcocho con yogur griego",
+          ingredients: [
+            "yogur griego",
+            "harina",
+            "levadura",
+            "aceite de girasol",
+            "limon",
+            "naranjas",
+            "frutos del bosque congelados",
+            "pepitas de chocolate",
+          ],
         },
         {
           name: "Comida",
           description: "broccoli gratinado",
+          ingredients: ["broccoli", "queso"],
         },
         {
           name: "Cena",
           description:
             "picoteo de gazpacho, bocadillo pollo rebozado, hummus y aceitunas",
+          ingredients: [
+            "pan",
+            "gazpacho",
+            "hummus",
+            "aceitunas",
+            "pechuga de pollo",
+            "huevos",
+            "harina",
+            "pan rallado",
+          ],
         },
       ],
     },
   ];
-  
+
   return (
     <ChakraProvider>
       <Box w="370px" m="auto" p={4} align="left">
